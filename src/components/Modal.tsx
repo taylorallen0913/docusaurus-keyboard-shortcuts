5ebf27f4817fc99d5233c46b3f3417dbc02b0fa4
Taylor Allen <taylorallen0913@gmail.com>
2020-06-30 20:02:01 -0700

    cleaned modal up

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import './modal.css'
 
-const Modal = (isModalVisible: any, setIsModalVisible: any) => {
+const Modal = ({ isModalVisible, setIsModalVisible }: any) => {
     return (
-        <div className="card-demo">
-            <div className="card">
-                <div className="card__header">
-                    <h3>Lorem Ipsum</h3>
-                </div>
-                <div className="card__body">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-                        suspendisse ultrices gravida.
+        <Fragment>
+            {
+                isModalVisible ?
+                    <div className="card-modal">
+                        <div className="card-demo">
+                            <div className="card">
+                                <div className="card__header">
+                                    <h3>Lorem Ipsum</h3>
+                                </div>
+                                <div className="card__body">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+                                        suspendisse ultrices gravida.
                     </p>
-                </div>
-                <div className="card__footer">
-                    <button className="button button--secondary button--block">See All</button>
-                </div>
-            </div>
-        </div>
+                                </div>
+                                <div className="card__footer">
+                                    <button className="button button--secondary button--block">See All</button>
+                                </div>
+                            </div>
+                        </div>
+                        <button onClick={() => setIsModalVisible(false)}>Hide Modal</button>
+                    </div> : <button onClick={() => setIsModalVisible(true)}>Set Modal Visible</button>
+            }
+        </Fragment>
     )
-    // return <div><h1>Hello world 2</h1></div>
 };
 
 export default Modal;
--- src/contexts/ShortcutContext.tsx
+++ src/contexts/ShortcutContext.tsx
@@ -6,14 +6,11 @@ export const ShortcutContext = createContext('hello');
 
 export const ShortcutContextProvider = ({ children }: any) => {
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-
   const keyboardShortcut = useKeyboardShortcut();
+
   return (
     <ShortcutContext.Provider value={"yes"}>
-      {
-        isModalVisible ?
-          <Modal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible} /> : <button onClick={() => setIsModalVisible(true)}>Set Modal Visible</button>
-      }
+      <Modal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible} />
       {children}
     </ShortcutContext.Provider>
   );
