dc0f6d5ba161861ec275671960e6c8feef5a348c
Taylor Allen <taylorallen0913@gmail.com>
2020-06-30 23:22:38 -0700

    refractored keybind object to be more semantic

--- src/contexts/ShortcutContext.tsx
+++ src/contexts/ShortcutContext.tsx
@@ -10,11 +10,21 @@ const modalReducer = (state: any, value: any) => value;
 export const ShortcutContextProvider = ({ children }: any) => {
   const [isModalVisible, setIsModalVisible] = useReducer(modalReducer, false);
 
-  let keybinds: any = {};
-  // Modal
-  keybinds[['Shift ?', 'Shift ~'].toString()] = () => setIsModalVisible(true);
-  // Alert
-  keybinds[['Shift M'].toString()] = () => alert('Alert!');
+  const keybinds: any = {
+    modal: {
+      keyCombo: [['Shift ?'].toString()],
+      action: () => setIsModalVisible(true)
+    },
+    alert: {
+      keyCombo: [['Shift M'].toString()],
+      action: () => alert('Alert!')
+    }
+  };
+
+  // keybinds[['Shift ?'].toString()] = () => setIsModalVisible(true)
+  // keybinds[['Shift M'].toString()] = () => alert('Alert!')
+
+  console.log(keybinds)
 
   const keyboardShortcut = useKeyboardShortcut(keybinds);
 
--- src/utils/keybinds.ts
+++ src/utils/keybinds.ts
@@ -1,15 +1,11 @@
 export const checkKeybinds = (state: Set<string>, keybinds: any) => {
-  // Iterate through each keybind
-  Object.keys(keybinds).forEach((keybind) => {
-    const parsedKeybinds = keybind.split(',');
+  Object.values(keybinds).forEach((keybind: any) => {
+    const parsedKeybinds = keybind.keyCombo[0].split(',');
     const parsedState = Array.from(state).toString().split(',').join(' ');
-    parsedKeybinds.forEach((parsedKeybind) => {
-      // If singular keybind and input are same length
+    parsedKeybinds.forEach((parsedKeybind: any) => {
       if (parsedKeybind.split(' ').length === state.size) {
-        // If keybind is the same as input
         if (parsedKeybind === parsedState) {
-          // Execute keybind action
-          keybinds[keybind]();
+          keybind.action();
         }
       }
     });
