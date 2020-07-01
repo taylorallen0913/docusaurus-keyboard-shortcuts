87ed5ef1659d5943b1b5a80ad8e456b8402e7ceb
Taylor Allen <taylorallen0913@gmail.com>
2020-07-01 14:29:22 -0700

    switched pagination to pill navigation in modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import clsx from 'clsx'
 
 import styles from './styles.module.css';
 
@@ -6,12 +7,12 @@ const parseKeyCombo = (keyCombo: string) => keyCombo.split(' ').join(" + ");
 
 function KeybindDisplay({ name, keyCombos }: any) {
     return (
-        <div className="shortcuts-content-container">
-            <h1 className="shortcuts-title">{name}</h1>
-            <div className="shortcuts-spacing" />
+        <div className={styles.contentContainer}>
+            <h1 className={styles.shortcutsTitle}>{name}</h1>
+            <div className={styles.shortcutsSpacing} />
             {
                 keyCombos.map((combo: any) => (
-                    <p className="shortcuts-key-combo" key={combo.toString()}>
+                    <p className={styles.shortcutsKeyCombo} key={combo.toString()}>
                         {parseKeyCombo(combo)}
                     </p>
                 ))
@@ -56,58 +57,60 @@ function Modal({ setIsModalVisible, keybinds }: any): JSX.Element {
 
 
     return (
-        <div className="shortcuts-modal-container">
-            <div className="shortcuts-modal-background" />
-            <div className="shortcuts-modal-content">
-                <div className="card">
-                    <div className="card__header shortcuts-header-container">
-                        <h1 className="shortcuts-header">Docusaurus Keyboard Shortcuts</h1>
+        <div className={styles.shortcutsModalContainer}>
+            <div className={styles.shortcutsModalBackground} />
+            <div className={styles.shortcutsModalContent}>
+                <div className="card card--full-height">
+                    <div className={clsx("card__header", styles.shortcutsHeaderContainer)}>
+                        <h1 className={styles.shortcutsHeader}>Docusaurus Keyboard Shortcuts</h1>
                     </div>
                     <div>
-                        <ul className="pills pills--block">
-                            <li className="pills__item pills__item--active shortcuts-pills">General</li>
-                            <li className="pills__item shortcuts-pills">Beta</li>
-                            <li className="pills__item shortcuts-pills">Gamma</li>
-                            <li className="pills__item shortcuts-pills">Zeta</li>
-                            <li className="pills__item shortcuts-pills">Zeta</li>
-                            <li className="pills__item shortcuts-pills">Zeta</li>
-                            <li className="pills__item shortcuts-pills">Zeta</li>
-                            <li className="pills__item shortcuts-pills">Zeta</li>
+                        <ul className={clsx("pills pills--block", styles.shortcutsPills)}>
+                            <li className={clsx('pills__item pills__item--active', styles.shortcutsPillItem)}>General</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Beta</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Gamma</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Zeta</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Zeta</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Zeta</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Zeta</li>
+                            <li className={clsx('pills__item', styles.shortcutsPillItem)}>Zeta</li>
                         </ul>
                     </div>
-                    <div className="card__body row">
-                        <div className="col shortcuts-container">
-                            {
-                                colOneList &&
-                                colOneList.map((keybind: any) => (
-                                    <KeybindDisplay
-                                        key={keybind.name.toString()}
-                                        name={keybind.name}
-                                        keyCombos={keybind.keyCombo}
-                                    />
-                                ))
-                            }
+                    <div className={styles.shortcutsBodyContainer}>
+                        <div className="card__body row">
+                            <div className={clsx('col', styles.shortcutsContainer)}>
+                                {
+                                    colOneList &&
+                                    colOneList.map((keybind: any) => (
+                                        <KeybindDisplay
+                                            key={keybind.name.toString()}
+                                            name={keybind.name}
+                                            keyCombos={keybind.keyCombo}
+                                        />
+                                    ))
+                                }
+                            </div>
+                            <div className={clsx('col', styles.shortcutsContainer)}>
+                                {
+                                    colTwoList &&
+                                    colTwoList.map((keybind: any) => (
+                                        <KeybindDisplay
+                                            key={keybind.name.toString()}
+                                            name={keybind.name}
+                                            keyCombos={keybind.keyCombo}
+                                        />
+                                    ))
+                                }
+                            </div>
                         </div>
-                        <div className="col shortcuts-container">
-                            {
-                                colTwoList &&
-                                colTwoList.map((keybind: any) => (
-                                    <KeybindDisplay
-                                        key={keybind.name.toString()}
-                                        name={keybind.name}
-                                        keyCombos={keybind.keyCombo}
-                                    />
-                                ))
-                            }
-                        </div>
-                    </div>
-                    <div className="card__footer">
-                        <div>
-                            <button
-                                className="button button--secondary button--block shortcuts-dismiss-button"
-                                onClick={() => setIsModalVisible(false)}>
-                                Dismiss
-                        </button>
+                        <div className="card__footer">
+                            <div>
+                                <button
+                                    className={clsx('button button--primary button--block', styles.shortcutsDismissButton)}
+                                    onClick={() => setIsModalVisible(false)}>
+                                    Dismiss
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
